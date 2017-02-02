e53f855b47a67f9420d39676a5625a65ca2a8a44
hong-zhou <hong-zhou@github.com>
2017-02-02 00:59:01 -0600

    first hadoop commit

---
+++ wordCount/src/main/java/com/hongzhou/wordCount/App.java
@@ -0,0 +1,13 @@
+package com.hongzhou.wordCount;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
